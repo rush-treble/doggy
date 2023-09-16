2f0a99faf52f261e233ea94cf828240eaa24ddbb
rush-treble <rush.treble@gmail.com>
2023-09-16 17:06:50 -0500

    fix formatting on window.d

--- src/doggy/window.d
+++ src/doggy/window.d
@@ -7,8 +7,12 @@ SDL_Window* window_ptr;
 int create(string title, int width, int height)
 {
     // Create the SDL window
-    window_ptr = SDL_CreateWindow(cast(const(char)*) title, SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
+    window_ptr = SDL_CreateWindow(cast(const(char)*) title, 
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED, 
+            width, 
+            height, 
+            SDL_WINDOW_SHOWN);
 
     // Return 0 on fail, 1 on succeed 
     if (window_ptr == null)
