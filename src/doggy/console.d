9bcc1ff96adb0160e01cd6659791617914a53765
rush-treble <rush.treble@gmail.com>
2023-09-24 18:32:15 -0500

    improve console logging utility

--- src/doggy/console.d
+++ src/doggy/console.d
@@ -2,18 +2,19 @@ module doggy.console;
 
 import std.stdio;
 import bindbc.sdl;
+import std.conv;
 
-void info(string text)
+void info(string text, string filename = __FILE__, int line = __LINE__)
 {
-    writeln("[INFO @ ", SDL_GetTicks(), "] ", text);
+    writeln("[INFO @ ", SDL_GetTicks(), " - ", filename, ":", to!string(line), " ] ", text);
 }
 
-void warning(string text)
+void warning(string text, string filename = __FILE__, int line = __LINE__)
 {
-    writeln("[WARN @ ", SDL_GetTicks(), "] ", text);
+    writeln("[WARN @ ", SDL_GetTicks(), " - ", filename, ":", to!string(line), " ] ", text);
 }
 
-void error(string text)
+void error(string text, string filename = __FILE__, int line = __LINE__)
 {
-    writeln("[ERROR!!!!! @ ", SDL_GetTicks(), "] ", text);
+    writeln("[ERROR @ ", SDL_GetTicks(), " - ", filename, ":", to!string(line), " ] ", text);
 }
